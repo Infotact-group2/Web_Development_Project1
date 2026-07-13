9b5a6cb178484555860db55fab282158a4d2a461
akhand4025 <thakurakhand4025@gmail.com>
2026-07-13 21:51:43 +0530

    Update FleetDash backend APIs

--- backend/src/controllers/telemetrycontroller.js
+++ backend/src/controllers/telemetrycontroller.js
@@ -42,8 +42,57 @@ const gettelemetry = async (req, res) => {
     }
 };
 
+const deletetelemetry=async (req,res)=>{
+    try{
+      const deletevehicle=await vehicles.findOneAndDelete({ vehicleId: req.params.id })
+        if(!deletevehicle){
+            return res.status(404).json({
+                success: false,
+                message: "Vehicle not found"
+            });
+        }
+          return res.status(200).json({
+            success: true,
+            message: "Telemetry deleted successfully"
+        });
+    }catch(err){
+        return res.status(400).json({msge:"something went wrong: "+ err.message})
+    }
+}
+
+
+const updatetelemetry = async (req, res) => {
+    try {
+        const updatedvehicle = await vehicles.findOneAndUpdate(
+            { vehicleId: req.params.id },
+            req.body,
+            { new: true }
+        );
+
+        if (!updatedvehicle) {
+            return res.status(404).json({
+                success: false,
+                message: "Vehicle not found"
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            data: updatedvehicle
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+};
+
 
 module.exports = {
     addtelemetry,
-    gettelemetry
+    gettelemetry,
+    deletetelemetry,
+    updatetelemetry
 }
--- backend/src/routes/telemetryroutes.js
+++ backend/src/routes/telemetryroutes.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const router = express.Router()
-const { addtelemetry, gettelemetry } = require("../controllers/telemetrycontroller")
+const { addtelemetry, gettelemetry,deletetelemetry, updatetelemetry } = require("../controllers/telemetrycontroller")
 
 router.post('/addtelemetry', addtelemetry)
 router.get('/gettelemetry', gettelemetry)
+router.delete('/deletetelemetry/:id',deletetelemetry)
+router.patch('/updatetelemetry/:id',updatetelemetry)
 
 module.exports = router
